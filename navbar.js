03c756bd6d7d79c0c9f8b3e69bb381c963994956
mitstack <deepabhawnani@yahoo.com>
2022-07-17 10:41:55 -0400

    final

--- navbar.js
+++ navbar.js
@@ -8,7 +8,7 @@ function NavBar({isLoggedin, setLoggedin}){
   return(
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow p-3 mb-5 bg-body rounded">
-      <a className="navbar-brand" href="#">Ash Bank</a>
+      <a className="navbar-brand" href="#">Bad bank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={handleNavCollapse}>
         <span className="navbar-toggler-icon"></span>
       </button>
